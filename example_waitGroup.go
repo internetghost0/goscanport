3a523ffc42f6dd0b25c1bf9dc2e68a38be152cce
zer0 <internetghost0@protonmail.com>
2022-12-29 04:33:54 +0000

    add comments

--- example_waitGroup.go
+++ example_waitGroup.go
@@ -6,39 +6,60 @@ import (
     "sync"
 )
 
+func main() {
+    // host to scan
+    host := "127.0.0.1"
+    // ports to scan
+    portStart := 1
+    portEnd   := 65535
+
+    // result
+    openPorts := []int{}
+    // for locking/unlocking openPorts variable
+    mu := sync.Mutex{}
+    // for managing goroutines
+    var wg sync.WaitGroup
+
+    for port := portStart; port <= portEnd; port++ {
+        // increase task counter (task is going to start)
+        wg.Add(1)
+
+        // create a new thread for scan a port
+        go func(port int) {
+            // if port is open
+            if isPortOpen(host, port) {
+                mu.Lock()
+                // append port to result
+                openPorts = append(openPorts, port)
+                mu.Unlock()
+            }
+            //decrease task counter (task is done)
+            wg.Done()
+        }(port)
+    }
+
+    // wait for tasks to done
+    wg.Wait()
+    // print result
+    fmt.Println(openPorts)
+}
+
 func isPortOpen(host string, port int) bool {
+
     hostPort := fmt.Sprintf("%s:%d", host, port)
+
+    // resolve address
     tcpAddr, err := net.ResolveTCPAddr("tcp4", hostPort)
     if err != nil {
         return false
     }
+
+    // try to connect
     conn, err := net.Dial("tcp", tcpAddr.String())
     if err != nil {
         return false
     }
     defer conn.Close()
+
     return true
-}
-
-func main() {
-    host := "127.0.0.1"
-    portStart := 1
-    portEnd   := 65535
-
-    openPorts := []int{}
-    mu := sync.Mutex{}
-    var wg sync.WaitGroup
-    for port := portStart; port <= portEnd; port++ {
-        wg.Add(1)
-        go func(port int) {
-            if isPortOpen(host, port) {
-                mu.Lock()
-                openPorts = append(openPorts, port)
-                mu.Unlock()
-            }
-            wg.Done()
-        }(port)
-    }
-    wg.Wait()
-    fmt.Println(openPorts)
 }
--- main.go
+++ main.go
@@ -6,6 +6,45 @@ import (
     "sync"
 )
 
+func main() {
+    // host addr
+    host := "127.0.0.1"
+    // ports for scan 
+    portStart := 1
+    portEnd   := 65535
+    // threads count
+    threads := 20
+
+    // result
+    openPorts := []int{}
+    // for locking/unlocking openPorts
+    mu := sync.Mutex{}
+    // for managing threads
+    sem := make(chan bool, threads)
+
+    for port := portStart; port <= portEnd; port++ {
+        sem <- true // like wg.Add(1)
+        go func(port int) {
+            // if port is open
+            if isPortOpen(host, port) {
+                mu.Lock()
+                // add port to result
+                openPorts = append(openPorts, port)
+                mu.Unlock()
+            }
+            <- sem // like wg.Done() & if `sem` have full capacity then wait for goroutines to finish
+        }(port)
+    }
+
+    // like wg.Wait()
+    for i := 0; i < cap(sem); i++ {
+        sem <- true
+    }
+
+    // print result
+    fmt.Println(openPorts)
+}
+
 func isPortOpen(host string, port int) bool {
     hostPort := fmt.Sprintf("%s:%d", host, port)
 
@@ -23,37 +62,4 @@ func isPortOpen(host string, port int) bool {
     defer conn.Close()
 
     return true
-}
-
-func main() {
-    // host addr
-    host := "127.0.0.1"
-    // ports for scan (portStart-portEnd)
-    portStart := 1
-    portEnd   := 65535
-    // threads count
-    threads := 20
-
-    openPorts := []int{}
-    mu := sync.Mutex{}
-    sem := make(chan bool, threads)
-
-    for port := portStart; port <= portEnd; port++ {
-        sem <- true // like wg.Add(1)
-        go func(port int) {
-            if isPortOpen(host, port) {
-                mu.Lock()
-                openPorts = append(openPorts, port)
-                mu.Unlock()
-            }
-            <- sem // like wg.Done() & if `sem` have full capacity then wait for goroutines to finish
-        }(port)
-    }
-
-    // like wg.Wait()
-    for i := 0; i < cap(sem); i++ {
-        sem <- true
-    }
-
-    fmt.Println(openPorts)
 }
