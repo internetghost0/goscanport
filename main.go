8c14a311224863a5c87e42552e8fad3c3c09e6c5
zer0 <internetghost0@protonmail.com>
2022-12-29 04:12:40 +0000

    add comments

--- main.go
+++ main.go
@@ -8,40 +8,52 @@ import (
 
 func isPortOpen(host string, port int) bool {
     hostPort := fmt.Sprintf("%s:%d", host, port)
+
+    // resolve address
     tcpAddr, err := net.ResolveTCPAddr("tcp4", hostPort)
     if err != nil {
         return false
     }
+
+    // try to connect
     conn, err := net.Dial("tcp", tcpAddr.String())
     if err != nil {
         return false
     }
     defer conn.Close()
+
     return true
 }
 
 func main() {
+    // host addr
     host := "127.0.0.1"
+    // ports for scan (portStart-portEnd)
     portStart := 1
     portEnd   := 65535
+    // threads count
     threads := 20
 
     openPorts := []int{}
     mu := sync.Mutex{}
     sem := make(chan bool, threads)
+
     for port := portStart; port <= portEnd; port++ {
-        sem <- true
+        sem <- true // like wg.Add(1)
         go func(port int) {
             if isPortOpen(host, port) {
                 mu.Lock()
                 openPorts = append(openPorts, port)
                 mu.Unlock()
             }
-            <- sem
+            <- sem // like wg.Done() & can't be more count of threads than {threads} variable
         }(port)
     }
+
+    // like wg.Wait()
     for i := 0; i < cap(sem); i++ {
         sem <- true
     }
+
     fmt.Println(openPorts)
 }
